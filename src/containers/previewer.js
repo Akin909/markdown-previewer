5bbcf19e208435beeaa1bfb264ab743a703308ef
Akin909 <akin.sowemimo@gmail.com>
2017-04-13 23:23:05 +0100

    Add emoji dependencies and emojification function[wip]

--- src/containers/previewer.js
+++ src/containers/previewer.js
@@ -3,27 +3,29 @@ import styled from 'styled-components';
 import marked from 'marked';
 import { Container } from '../styled/general.js';
 import highlight from 'highlight.js';
-// import pygments from 'pygmentize-bundled';
+import emojis from 'emojilib';
 
 
 const MarkdownInput = styled.textarea`
-  width: 80%;
-  height: 30%;
-  margin: 1rem;
-  border: none;
-  box-shadow: 0px 1px 1px grey;
-  padding: 1rem;
-  font-size: 1.4rem;
+background: #FCEFEF;
+width: 80%;
+height: 30%;
+margin: 1rem;
+border: none;
+box-shadow: 0px 1px 1px grey;
+padding: 1rem;
+font-size: 1.4rem;
 `;
 const MarkdownOutput = styled.div`
-  font-size: 1.4rem;
-  padding: 1rem;
-  width: 80%;
-  background: ${ props => props.markdown ? 'white' : '#A1FCDF' };
-  height: 30%;
-  overflowY: auto;
-  margin: 1rem;
-  box-shadow: 0px 1px 1px grey;
+background: #FCEFEF;
+font-size: 1.4rem;
+padding: 1rem;
+width: 80%;
+background: ${ props => props.markdown ? 'white' : '#A1FCDF' };
+height: 30%;
+overflowY: auto;
+margin: 1rem;
+box-shadow: 0px 1px 1px grey;
 `;
 
 
@@ -41,8 +43,8 @@ class Previewer extends Component {
     };
 
 
-  this.handleChange = this.handleChange.bind(this);
-  this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 
@@ -68,17 +70,45 @@ class Previewer extends Component {
       sanitize: true,
       smartypants: true,
     });
-    const markdown = marked( this.state.value ); 
+    function tap(x){
+      console.log('tapping', x);
+      return x;
+    }
+
+    const markdown = tap(emojification(this.state));
     return { __html: markdown };
   }
   render() {
     return (
       <Container>
-        <MarkdownInput  type="text" value={this.state.value} onChange={this.handleChange} />
+        <MarkdownInput  type="text" autoFocus value={this.state.value} onChange={this.handleChange} />
         <MarkdownOutput markdown dangerouslySetInnerHTML={this.renderMarkdown()} />
       </Container>
-    );
+      );
   }
+}
+function emojification({value}) {
+    let markdown = marked(value);
+    if (value.includes(':')) {
+
+      //Regexp to find characters inside colons
+      const regEx = /(?:\:)\b(\w*)\b(?=\:)/g;
+      let preEmoji = value.match(regEx);
+
+      if (preEmoji) {
+        preEmoji = preEmoji[0].substring(1);
+
+        if (emojis.lib[preEmoji]) {
+
+          // console.log(emojis.lib[preEmoji].char);
+          const emoji = emojis.lib[preEmoji].char;
+            markdown = markdown.replace(':'+ preEmoji, emoji);   
+            return markdown;
+
+        }
+      }
+    } 
+      return markdown;
 }
 
 export default Previewer;
